be530b5d5acd547dbc1c94ed00fad81285e90c0a
hermesmc <hermesmc@gmail.com>
2021-06-29 11:34:55 -0300

    Mapeando uma unidade

---
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -0,0 +1,53 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+//Anotação da JPA
+@Entity
+@Table(name="PRODUTO")
+public class Produto {
+	
+	@Column(name="ID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="NOME")
+	private String nome;
+	@Column(name="DESCRICAO")
+	private String descricao;
+	@Column(name="PRECO")
+	private BigDecimal preco;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	
+	
+}
